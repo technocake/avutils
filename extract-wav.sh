941724c3419858c7d8d7b59b09de56e5854ad809
Robin Aaberg <robin.aaberg@knowit.no>
2018-11-30 15:12:09 +0100

    add mts2mkv converting tool that takes .MTS and converts it to .mkv - reencodes to pcm16b

--- extract-wav.sh
+++ extract-wav.sh
@@ -6,3 +6,12 @@ function extract_wav {
 	#	16b signed little endian pcm wav files, 2 channels at 44100Hz sampling rate.
 	ffmpeg.exe -i "$1" -acodec pcm_s16le -ar 44100 -ac 2 "$1.wav"
 }
+
+
+function mts2mkv {
+	# Converts MTS AVHDC video files to mkv videofiles
+	ffmpeg -i "$1" -acodec pcm_s16le -c:v copy "$1.mkv"
+}
+
+
+
